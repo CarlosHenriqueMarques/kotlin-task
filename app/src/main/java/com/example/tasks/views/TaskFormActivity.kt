4a40b8056d9db379c714c33d250b19e1dc7d803a
Carlos Maia <carlos.marques89@gmail.com>
2019-12-28 22:45:00 -0500

    Carregando o spinner de prioridades

--- app/src/main/java/com/example/tasks/views/TaskFormActivity.kt
+++ app/src/main/java/com/example/tasks/views/TaskFormActivity.kt
@@ -2,12 +2,28 @@ package com.example.tasks.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.example.tasks.R
+import com.example.tasks.business.PriorityBusiness
+import kotlinx.android.synthetic.main.activity_task_form.*
 
 class TaskFormActivity : AppCompatActivity() {
 
+    private lateinit var mBussinesPriority : PriorityBusiness
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task_form)
+
+        mBussinesPriority = PriorityBusiness(this)
+        loadPriorities()
+    }
+
+    private fun loadPriorities() {
+        val lstPrioritiesEntities = mBussinesPriority.getList()
+
+        //Converte toda a lista de entidade para uma lista de string em uma única linha
+        val lstPriorities = lstPrioritiesEntities.map { it.description }
+        val adapter = ArrayAdapter<String> (this,R.layout.support_simple_spinner_dropdown_item,lstPriorities)
+        spinnerPriority.adapter = adapter
     }
 }
