48a8ac23331aae3f40070cf277b789e40696b1fe
Carlos Maia <carlos.marques89@gmail.com>
2019-12-28 22:56:44 -0500

    Carregando o datepicker no cadastro da task

--- app/src/main/java/com/example/tasks/views/TaskFormActivity.kt
+++ app/src/main/java/com/example/tasks/views/TaskFormActivity.kt
@@ -1,21 +1,45 @@
 package com.example.tasks.views
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.DatePicker
 import com.example.tasks.R
 import com.example.tasks.business.PriorityBusiness
 import kotlinx.android.synthetic.main.activity_task_form.*
+import java.text.SimpleDateFormat
+import java.util.*
 
-class TaskFormActivity : AppCompatActivity() {
+class TaskFormActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener {
 
     private lateinit var mBussinesPriority : PriorityBusiness
+    private val mSimpleDateFormat : SimpleDateFormat = SimpleDateFormat("dd/MM/yyyy")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task_form)
 
         mBussinesPriority = PriorityBusiness(this)
         loadPriorities()
+
+        buttonDate.setOnClickListener {
+            openCalendar()
+        }
+    }
+
+    private fun openCalendar() {
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val dayOfMonth = c.get(Calendar.DAY_OF_MONTH)
+
+        DatePickerDialog(this,this,year,month,dayOfMonth).show()
+    }
+
+    override fun onDateSet(view: DatePicker?, years: Int, month: Int, dayOfMonths: Int) {
+        val calendar = Calendar.getInstance()
+        calendar.set(years,month,dayOfMonths)
+        buttonDate.text = mSimpleDateFormat.format(calendar.time)
     }
 
     private fun loadPriorities() {
@@ -26,4 +50,6 @@ class TaskFormActivity : AppCompatActivity() {
         val adapter = ArrayAdapter<String> (this,R.layout.support_simple_spinner_dropdown_item,lstPriorities)
         spinnerPriority.adapter = adapter
     }
+
+
 }
